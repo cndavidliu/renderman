9d0ab46131b2db435c9c6ffa3cb9cfed69924f35
mfmelody <davischahliu@gmail.com>
2014-04-29 14:50:27 +0800

    add config db module && test

---
+++ src/models/config.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column, Integer, Sequence, String, DateTime, ForeignKey
+from sqlalchemy.orm import relationship, backref
+from .meta import Model
+
+class Config(Model):
+	"""
+	config for each job
+	"""
+
+	__tablename__ = 'config'
+
+	id = Column(Integer, Sequence('config_id_seq'), primary_key = True)
+
+	#config info
+	instanceMem = Column(Integer, default = 200)
+	instanceCores = Column(Integer, default = 6)
+
+	#parameters for rendering
+	mapTaskCount = Column(Integer)
+	height = Column(Integer)
+	width = Column(Integer)
+
+	extraConfig = Column(String(255), default = '')
+
+	#Owner
+	job_id = Column(Integer, ForeignKey('job.id'), unique = True)
+	job = relationship('Job', backref = backref('configs', order_by = id))
+
+
+	def __repr__(self):
+		return "instanceMem = %d, instanceCores = %d" %(self.instanceCores, self.instanceMem)
+
+	def getRenderConfig(self):
+		return self.mapTaskCount, self.width, self.height
+
+	def setRenderConfig(self, mapTaskCount, width, height):
+		self.mapTaskCount = mapTaskCount
+		self.width = width
+		self.height = height
+
+	def __init__(self, jobId, instanceMem = 200, instanceCores = 6):
+		self.job_id = jobId
+		self.instanceMem = instanceMem
+		self.instanceCores = instanceCores
